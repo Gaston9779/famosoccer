3ac7b0e81a11e81740d38acbaf3c09c24f3b8d31
nicola-viola <nicthareal@gmail.com>
2026-09-07 23:02:59 +0200

    push

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = 'force-dynamic';
 import { Suspense } from "react";
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
